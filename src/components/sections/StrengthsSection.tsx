48dcf664e5ba93d0642a76837a27ba28bf1d8195
kabuki <tanuki5sanuki3@icloud.com>
2023-09-04 18:47:58 +0900

    add icons

--- src/components/sections/StrengthsSection.tsx
+++ src/components/sections/StrengthsSection.tsx
@@ -3,40 +3,40 @@ import Image from "next/image";
 export default function StrengthsSection() {
   const contentItems = [
     {
-      icon: "/images/icons/diversity_1.svg",
+      icon: "/images/icons/construction.svg",
       title: "Custom",
       description:
         "Our content filter offers unparalleled customization, allowing you to tailor your online experience to your exact preferences.",
     },
     {
-      icon: "https://via.placeholder.com/200x200",
+      icon: "/images/icons/translate.svg",
       title: "Global",
       description:
         "Experience a truly global online environment with our content filter, supporting multiple languages to ensure a comfortable browsing experience for everyone.",
     },
     {
-      icon: "https://via.placeholder.com/200x200",
+      icon: "/images/icons/diversity_1.svg",
       title: "Filtering",
       description:
         "We've got you covered across a wide range of websites! Our content filter supports a vast array of platforms, ensuring a safer internet experience wherever you go.",
     },
     {
-      icon: "https://via.placeholder.com/200x200",
+      icon: "/images/icons/devices.svg",
       title: "Feature 4",
       description:
         "Enjoy seamless protection across multiple platforms with our content filter. Whether you're on your computer, smartphone, or tablet, we've got your back.",
     },
     {
-      icon: "https://via.placeholder.com/200x200",
+      icon: "/images/icons/security.svg",
       title: "Feature 5",
       description:
         "Your privacy matters to us. Our content filter not only blocks explicit content but also ensures your online activities remain private, creating a safer and more secure browsing experience.",
     },
     {
-      icon: "https://via.placeholder.com/200x200",
+      icon: "/images/icons/menstrual_health.svg",
       title: "Feature 6",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "The app does more than just filtering - it also has features for your mental health. It protects the health of your life.",
     },
   ];
 
--- src/components/sections/TestimonialSection.tsx
+++ src/components/sections/TestimonialSection.tsx
@@ -1,38 +1,62 @@
 import React from "react";
+import Image from "next/image";
 
 export default function TestimonialSection() {
   return (
     <div className="container my-24 mx-auto md:px-6">
       <section className="mb-32 text-center">
-        <div className="lg:flex">
-          {userCard()}
-          {userCard()}
-          {userCard()}
+        <div className="lg:flex ">
+          {userCard(
+            "Emily",
+            "Teacher",
+            "I've been using 'zeboot' for a while now, and it's been a game-changer for me. As a teacher, maintaining focus and a healthy online environment is crucial. ",
+            "/images/sample/sample1.png"
+          )}
+           {userCard(
+            "David",
+            "Software Developer",
+            "'zeboot' has become an essential part of my digital life. As a software developer, I spend a lot of time online, and it's easy to get distracted.",
+            "/images/sample/sample1.png"
+          )}
+           {userCard(
+            "Sarah",
+            "Marketing Manager",
+            "I can't thank 'zeboot' enough for keeping me productive and safeguarding my online experience. As a marketing manager, I need a clear mind to come up with creative ideas. ",
+            "/images/sample/sample1.png"
+          )}
+          
         </div>
       </section>
     </div>
   );
 }
 
-function userCard() {
+function userCard(
+  name: string,
+  job: string,
+  comment: string,
+  imageSrc: string
+) {
   return (
     <div className="mb-12 md:mb-0">
       <div className="mb-6 flex justify-center items-center">
-        <img
-          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+        <Image
+          src={imageSrc}
           className="w-10 h-10 rounded-full shadow-lg dark:shadow-black/20"
+          alt={name}
+          width={200}
+          height={200}
         />
         <div className="ml-4 ">
-          <h5 className="text-lg font-bold">Maria Smantha</h5>
-          <h6 className="font-medium text-primary dark:text-primary-400">
-            Web Developer
-          </h6>
+          <h3 className="text-lg font-bold">{name}</h3>
+          <h3 className="font-medium text-primary dark:text-primary-400">
+            {job}
+          </h3>
         </div>
       </div>
 
-      <p className="mb-4 lg:ma-w-1/2 lg:text-sm max-w-1/4 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id
-        officiis hic tenetur quae quaerat ad velit ab hic.
+      <p className="mb-4 lg:max-w-1/2 lg:text-sm max-w-1/4 text-sm">
+        {comment}
       </p>
       <div className="rating">
         <input type="radio" name="rating-1" className="mask mask-star" />

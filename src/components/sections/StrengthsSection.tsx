7370a24ea82f067c1deec65e1e62ab763b910820
kabuki <tanuki5sanuki3@icloud.com>
2023-09-04 16:29:56 +0900

    fix file name

--- src/components/sections/StrengthsSection.tsx
+++ src/components/sections/StrengthsSection.tsx
@@ -3,34 +3,34 @@ import Image from "next/image";
 export default function StrengthsSection() {
   const contentItems = [
     {
-      icon: "https://via.placeholder.com/200x200",
-      title: "Feature 1",
+      icon: "/images/icons/diversity_1.svg",
+      title: "Custom",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "Our content filter offers unparalleled customization, allowing you to tailor your online experience to your exact preferences.",
     },
     {
       icon: "https://via.placeholder.com/200x200",
-      title: "Feature 2",
+      title: "Global",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "Experience a truly global online environment with our content filter, supporting multiple languages to ensure a comfortable browsing experience for everyone.",
     },
     {
       icon: "https://via.placeholder.com/200x200",
-      title: "Feature 3",
+      title: "Filtering",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "We've got you covered across a wide range of websites! Our content filter supports a vast array of platforms, ensuring a safer internet experience wherever you go.",
     },
     {
       icon: "https://via.placeholder.com/200x200",
       title: "Feature 4",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "Enjoy seamless protection across multiple platforms with our content filter. Whether you're on your computer, smartphone, or tablet, we've got your back.",
     },
     {
       icon: "https://via.placeholder.com/200x200",
       title: "Feature 5",
       description:
-        "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.",
+        "Your privacy matters to us. Our content filter not only blocks explicit content but also ensures your online activities remain private, creating a safer and more secure browsing experience.",
     },
     {
       icon: "https://via.placeholder.com/200x200",
@@ -55,7 +55,7 @@ export default function StrengthsSection() {
             <h2 className="lg:text-xl text-md font-semibold  ml-2 flex items-center">
               {" "}
               {/* Added 'flex' and 'items-center' class */}
-              {item.title}
+              {""}
             </h2>
           </div>
           <p className="text-gray-600 lg:text-sm text-xs">{item.description}</p>

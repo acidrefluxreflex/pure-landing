9cd4e67974dcc65ba7fd9cb981ffc0bc49fcd86e
kabuki <tanuki5sanuki3@icloud.com>
2023-09-06 13:59:44 +0900

    fix &apos;

--- src/components/sections/GetStartSection.tsx
+++ src/components/sections/GetStartSection.tsx
@@ -6,7 +6,7 @@ export default function GetStartSection() {
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-6">Start New Life</h2>
         <p className="text-gray-600 text-lg mb-8">
-          Don't wait any longer to safeguard your concentration and mental
+          Don&apos;t wait any longer to safeguard your concentration and mental
           health. Click the link below to download Zeboot and take the first
           step towards a more secure online experience. With Zeboot by your
           side, your new life begins today!
